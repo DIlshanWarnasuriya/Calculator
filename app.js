bf28f5bb11093fff70a350d84f0fa0575bdbfa58
DilshanWarnasuriya <dilshan.se.dew@gmail.com>
2024-02-27 23:38:30 +0530

    Add functions

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-function Addtion(){
-    let x =+ document.getElementById("txtNum1").value;
-    let y =+ document.getElementById("txtNum2").value;
+let x =+ document.getElementById("txtNum1").value;
+let y =+ document.getElementById("txtNum2").value;
+
+function Addition(){    
+    document.getElementById("Output").innerHTML="Output is - " + x + " + " + y + " = "+ (x+y);
+}
+
+function Substraction(){
+    document.getElementById("Output").innerHTML="Output is - " + x + " - " + y + " = "+ (x-y);
+}
+
+function Devide(){
+    document.getElementById("Output").innerHTML="Output is - " + x + " / " + y + " = "+ (x/y);
+}
+
+function Multiply(){
+    document.getElementById("Output").innerHTML="Output is - " + x + " * " + y + " = "+ (x*y);
 }
